60ade5bf1cb099fafff40fbf65ebe7d67493bfb3
Christian Johansson <christian@cvj.se>
2016-04-22 06:53:00 +0200

    Initial commit

---
+++ vimeovideo/plugin.js
@@ -0,0 +1,49 @@
+/**
+ *
+ */
+
+CKEDITOR.plugins.add('vimeovideo',
+{
+
+        init: function(editor)
+        {
+
+            CKEDITOR.dialog.add("vimeovideoDialog", function (c)
+            {
+                return{title: 'Enter Vimeo video URL', minWidth: 400, minHeight: 75, contents: [
+                    {id: "tab-basic", label: "Basic Settings", elements: [
+                        {type: "text", id: "vimeovideoURL", label: 'URL' }
+                    ]}
+                ], onOk: function ()
+                {
+                    var b = this.getValueOf("tab-basic", "vimeovideoURL").trim().match(/vimeo.com\/([^&$]+)/i);
+                    if (null == b || "" == b || "" == b[0] || "" == b[1]) {
+                        return alert('URL was invalid! It should be similar to \n\n\t https://vimeo.com/123456 \n\n Try again!'),
+                            !1;
+                    }
+
+                    var a = c.document.createElement("iframe");
+                    a.setAttribute("width", "500");
+                    a.setAttribute("height", "281");
+                    a.setAttribute("src", "https://player.vimeo.com/video/" + b[1]);
+                    a.setAttribute("frameborder", "0");
+                    a.setAttribute("allowfullscreen", "1");
+                    c.insertElement(a)
+
+                }}
+            });
+
+            // Add command
+            editor.addCommand('vimeovideoDialog', new CKEDITOR.dialogCommand("vimeovideoDialog"));
+
+            /// Add UI toolbar button
+            editor.ui.addButton('vimeovideo',
+                {
+                    label: 'Embed Vimeo video',
+                    command: 'vimeovideoDialog',
+                    icon: this.path + 'images/vimeo-16x16.png'
+                });
+
+        }
+
+    });
